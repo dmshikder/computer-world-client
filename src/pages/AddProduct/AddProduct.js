f53524c0c516dcd2494944a7702433b08baa82e3
dmshikder <dmshikder@gmail.com>
2022-07-05 06:55:28 +0600

    add product form designed

--- src/pages/AddProduct/AddProduct.js
+++ src/pages/AddProduct/AddProduct.js
@@ -1,24 +1,31 @@
 import { useForm } from "react-hook-form";
 const AddProduct = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    return (
-        <div>
-            <h2>please add product</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="w-50 mx-auto">
+      <h2>please add product</h2>
+      <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
 
-        </div>
-    );
+      <input className="mb-2" placeholder="name"  {...register("name")} />
+        <textarea className="mb-2" placeholder="description"  {...register("description")} />
+       
+        <input className="mb-2" placeholder="supplier"  {...register("supplier")} />
+        <input className="mb-2" placeholder="quantity"  type="number" {...register("quantity")} />
+        <input className="mb-2" placeholder="price"  type="number" {...register("price")} />
+        <input className="mb-2" placeholder="img url"  {...register("img")} />
+
+     
+
+        <input className="mb-5" type="submit"  value="Add product"/>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
